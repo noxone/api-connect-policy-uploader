6d22ccfe1a1e5a764ee236a023713e3ae37e9f48
Olaf Neumann <git@olafneumann.org>
2018-08-22 00:06:36 +0200

    Improve exception handling

--- src/main/java/org/olafneumann/apic/policy/PolicyUploader.java
+++ src/main/java/org/olafneumann/apic/policy/PolicyUploader.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.olafneumann.apic.policy.StartOptions.Credentials;
 
 public class PolicyUploader {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) {
 		// parse command line options
 		StartOptions options = StartOptions.parse(args);
 		// if command line options fail: shut down
@@ -15,29 +15,39 @@ public class PolicyUploader {
 		}
 
 		final Credentials credentials = options.getCredentials();
-		final ApiConnectClient client = new ApiConnectClient(
-			options.getServer(),
-			options.getOrganization(),
-			options.getCatalog(),
-			options.isIgnoreSsl());
+		final ApiConnectClient client = new ApiConnectClient(options.getServer(), options.getOrganization(),
+				options.getCatalog(), options.isIgnoreSsl());
 
 		// Log in to server
-		boolean loggedIn = client.login(credentials.getUsername(), credentials.getPassword());
-		if (!loggedIn) {
-			System.err.println(
-				String.format(
-					"Unable to log in to server[%s] using username[%s].",
-					options.getServer(),
-					credentials.getUsername()));
+		final boolean loggedIn;
+		try {
+			loggedIn = client.login(credentials.getUsername(), credentials.getPassword());
+		} catch (IOException e) {
+			System.err.println("Unable to communicate with server while logging in.");
+			e.printStackTrace();
 			System.exit(1);
+			return;
+		}
+		if (!loggedIn) {
+			System.err.println(String.format("Unable to log in to server[%s] using username[%s].", options.getServer(),
+					credentials.getUsername()));
+			System.exit(2);
 			return;
 		}
 
 		// Do the upload
-		Optional<String> message = client.uploadPolicy(options.getPolicyFile());
+		final Optional<String> message;
+		try {
+			message = client.uploadPolicy(options.getPolicyFile());
+		} catch (IOException e) {
+			System.err.println("Unable to communicate with server while uploading policy");
+			e.printStackTrace();
+			System.exit(3);
+			return;
+		}
 		if (message.isPresent()) {
 			System.err.println(String.format("Unable to upload policy.\r\n%s", message.get()));
-			System.exit(2);
+			System.exit(4);
 			return;
 		}
 	}
